7a853eea0e8d5a7e4cc5d136d5df69d1ddcf3c2a
biohow <biohow@163.com>
2021-09-03 14:28:48 +0800

    DEMO的提交初始化

---
+++ 代码/DEMO/src/helloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author : github
+ * @date : 2021/9/3 0003
+ * 主要用于仓库测试
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

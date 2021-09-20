96f2b16f88ecf32726c1ba2dd6d1722f1a9a8d6f
biohow <biohow@163.com>
2021-09-20 15:25:43 +0800

    个人博客项目
    Add springboot整合Swagger

--- 代码/blog/src/main/java/com/myblog/blog/controller/helloController.java
+++ 代码/blog/src/main/java/com/myblog/blog/controller/helloController.java
@@ -1,13 +1,15 @@
 package com.myblog.blog.controller;
 
-
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Api(value = "测试接口", tags = "用户管理相关的接口", description = "用户测试接口")
 @Controller
 public class helloController {
 
-    @RequestMapping("/index")
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String sayHello(){
         return "index";
     }
